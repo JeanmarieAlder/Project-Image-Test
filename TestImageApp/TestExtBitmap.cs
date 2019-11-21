8a953122c47d7c113de874784b651355558d8867
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2019-11-21 15:04:54 +0100

    Added image resource

---
+++ TestImageApp/TestExtBitmap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageEdgeDetection;
+
+
+namespace TestImageApp
+{
+    [TestClass]
+    public class UnitTest2
+    {
+
+        [TestMethod]
+        public void TestLaplacian3x3Filter()
+        {
+
+        }
+    }
+}
